272618714d3307525e924e6dbdea99fbb579c129
Moraish Kapoor <moraish.kapoor@hotmail.com>
2025-02-12 22:33:43 -0500

    Question Component taking shape

--- frontend/src/components/QuestionCard.tsx
+++ frontend/src/components/QuestionCard.tsx
@@ -27,12 +27,19 @@ export default function QuestionCard({ question_number, total_questions, questio
             {/* Options Container */}
             <div className="max-w-4xl  mx-auto">
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-6">
+
                     <OptionCard text={option_a} label="A" />
                     <OptionCard text={option_b} label="B" />
+
                     <OptionCard text={option_c} label="C" />
                     <OptionCard text={option_d} label="D" />
+
+
                 </div>
             </div>
+            {/* Navigation Container */}
+            <QuestionToggle />
+
         </div>
     );
 };
@@ -95,14 +102,36 @@ function Progress_chart({ quiz_status }) {
 const OptionCard = ({ text, label }) => {
     return (
         <button className="w-full text-left transition-all duration-200 group">
-            <div className="min-h-[4rem] p-4 bg-white rounded-lg border border-gray-200 hover:bg-blue-50 hover:border-blue-300 cursor-pointer focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 shadow-sm">
+            <div className="h-24 p-4 bg-white rounded-lg border border-gray-200 hover:bg-blue-50 hover:border-blue-300 cursor-pointer focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 shadow-sm">
                 <div className="flex gap-3">
                     <span className="flex-shrink-0 flex items-center justify-center w-8 h-8 rounded-full bg-gray-100 text-gray-700 font-semibold group-hover:bg-blue-100">
                         {label}
                     </span>
-                    <span className="text-gray-700 py-1">{text}</span>
+                    <span className="text-gray-700">{text}</span>
                 </div>
             </div>
         </button>
     );
 };
+
+function QuestionToggle() {
+    return (
+        <div className="flex justify-around">
+            <button className="flex flex-col items-center p-2 rounded-lg transition-colors duration-200 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 active:bg-gray-200">
+                <svg className="w-6 h-6 text-gray-800 transition-colors duration-200 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13" />
+                </svg>
+                <p className="pt-2 text-sm text-gray-500">Back</p>
+            </button>
+
+            <button className="flex flex-col items-center p-2 rounded-lg transition-colors duration-200 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 active:bg-gray-200">
+                <svg className="w-6 h-6 text-gray-800 transition-colors duration-200 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1" />
+                </svg>
+                <p className="pt-2 text-sm text-gray-500">Next</p>
+            </button>
+        </div>
+    )
+}
+
+
